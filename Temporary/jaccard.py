4f0ca920ffd3b5cad4907ebfc60b85486f2eaa9d
Michael Chary <mac389@gmail.com>
2015-03-08 13:13:45 -0400

    update

--- Temporary/jaccard.py
+++ Temporary/jaccard.py
@@ -13,19 +13,11 @@ bigramFinder("sanitized2")
 bigramFinder("sanitized3")
 
 def jaccard(file1, file2):
-    jaccardset1 = ""
-    with open(file1,'rb') as infile:
-        for word in infile:
-            jaccardset1 += (word + " ")
-    jaccardset2 = ""
-    with open(file2, 'rb') as infile:
-        for word in infile:
-            jaccardset2 += (word + " ")
-    set1 = set(jaccardset1.split())
-    set2 = set(jaccardset2.split())
-    print float(len(set1 & set2))/len(set1 | set2)
+    set1 = set(open(file1,'rb').read().splitlines())    
+    set2 = set(open(file2,'rb').read().splitlines())
+    return float(len(set1 & set2))/len(set1 | set2)
     
-jaccard('sanitizted', 'sanitized2')
-jaccard('sanitizted', 'sanitized3')
-jaccard('sanitized2', 'sanitized3')
+print jaccard('sanitizted', 'sanitized2')
+print jaccard('sanitizted', 'sanitized3')
+print jaccard('sanitized2', 'sanitized3')
 
